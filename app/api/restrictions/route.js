7a0f13f9166be53bf02f273d276913615fc2b3a8
Sajukhan1 <sajusohamoni12@gmail.com>
2025-07-16 17:07:42 +0600

    Update route.js

--- app/api/restrictions/route.js
+++ app/api/restrictions/route.js
@@ -1,16 +1,13 @@
-// app/api/restrictions/route.js
 import { NextResponse } from 'next/server';
 import { connectDB } from '@/app/database';
 import Video from '@/app/api/models/video';
 
 export async function GET() {
-  await connectDB();
-
   try {
+    await connectDB();
     const videos = await Video.find();
     return NextResponse.json(videos);
-  } catch (err) {
-    console.error('Error fetching videos:', err);
-    return NextResponse.json({ error: 'Failed to fetch videos' }, { status: 500 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
